0600c54c7fbef74b9350b635357ec1c2229be115
chel_dev <148582502+cheldevco@users.noreply.github.com>
2025-08-03 01:13:42 +0300

    4

---
+++ apps/meteor/client/views/root/MainLayout/Preload.tsx
@@ -0,0 +1,35 @@
+import { useUserId } from '@rocket.chat/ui-contexts';
+import type { ReactElement, ReactNode } from 'react';
+import { useEffect } from 'react';
+
+import { mainReady } from '../../../../app/ui-utils/client';
+import { RoomsCachedStore, SubscriptionsCachedStore, PublicSettingsCachedStore } from '../../../cachedStores';
+import { useReactiveVar } from '../../../hooks/useReactiveVar';
+import { isSyncReady } from '../../../lib/userData';
+import PageLoading from '../PageLoading';
+
+const Preload = ({ children }: { children: ReactNode }): ReactElement => {
+	const uid = useUserId();
+	const subscriptionsReady = SubscriptionsCachedStore.useReady();
+	const settingsReady = PublicSettingsCachedStore.useReady();
+	const userDataReady = useReactiveVar(isSyncReady);
+
+	const ready = !uid || (userDataReady && subscriptionsReady && settingsReady);
+
+	useEffect(() => {
+		mainReady.set(ready);
+	}, [ready]);
+
+	useEffect(() => {
+		SubscriptionsCachedStore.listen();
+		RoomsCachedStore.listen();
+	}, []);
+
+	if (!ready) {
+		return <PageLoading />;
+	}
+
+	return <>{children}</>;
+};
+
+export default Preload;
